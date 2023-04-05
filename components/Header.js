3c024813f4876aeb7ee7d86734cac7a2705bdb08
japeaux <lucascovatti@hotmail.com>
2023-04-05 14:58:07 -0300

    main

--- components/Header.js
+++ components/Header.js
@@ -2,17 +2,8 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import AppBar from './AppBar';
-import HomeIcon from '@mui/icons-material/Home';
-import ChatIcon from '@mui/icons-material/Chat';
-import RequestPageIcon from '@mui/icons-material/RequestPage';
-
-import { IconButton } from '@mui/material'
-
-
 
 function Header() {
-
-
 
   return (
     <div>
